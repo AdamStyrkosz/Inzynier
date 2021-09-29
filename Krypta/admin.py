16cbd28c2198bfb6f3f3317f6758c610501f5802
Adam <adam963110@gmail.com>
2021-09-29 22:06:34 +0200

    Dodano opis do modelu, zmieniono strone aktualnosci, poprawiono index

--- Krypta/admin.py
+++ Krypta/admin.py
@@ -1,7 +1,5 @@
-from Krypta.models import Wpis, Cryptocurrency, CryptoHistorical
+from Krypta.models import Wpis
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Wpis)
-admin.site.register(Cryptocurrency)
-admin.site.register(CryptoHistorical)
